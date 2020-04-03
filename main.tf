d46a0fc084f6cf81e03f9ea82881e087d30bce71
hslmith <kyrell@gmail.com>
2020-04-03 08:55:38 -0500

    Updated

--- main.tf
+++ main.tf
@@ -9,3 +9,20 @@ data "ibm_is_ssh_key" "sshkey1" {
   name = "${var.ssh-key-name}"
 }
 
+////////////////
+//Create VPC
+///////////////
+
+data "ibm_resource_group" "resource" {
+  name = "${var.resource_group}"
+}
+
+resource "ibm_is_vpc" "vpc1" {
+  name = "${var.vpc_name}"
+  address_prefix_management = "manual"
+  resource_group = "${data.ibm_resource_group.resource.id}"
+}
+
+////////////////
+//Create VPC CIDR and Zone Prefixes
+///////////////
