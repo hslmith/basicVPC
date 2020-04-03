1e1564be6af85e3b374377efbfb149c82e73745b
hslmith <kyrell@gmail.com>
2020-04-03 08:47:54 -0500

    Updated

--- variables.tf
+++ variables.tf
@@ -1,3 +1,33 @@
 ////////////////
 //Define Zones
 ////////////////
+
+variable "ibmcloud_region" {
+  description = "Preferred IBM Cloud region to use for your infrastructure"
+  default = "us-south"
+}
+
+variable "zone1" {
+  default = "us-south-1"
+  description = "Define the 1st zone of the region"
+}
+
+
+variable "vpc-name" {
+  default = "basic-vpc-demo"
+  description = "Name of your VPC"
+}
+
+
+////////////////
+// Define CIDR
+////////////////
+
+
+variable "vpc-address-prefix" {
+  default = "192.168.0.0/20"
+}
+
+variable "az1-prefix" {
+  default = "192.168.0.0/21"
+  description = "CIDR block to be used for zone 1"
